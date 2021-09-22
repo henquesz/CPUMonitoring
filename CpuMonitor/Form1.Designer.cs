a98de059f0e43866a70dd94d4754b65844f8115f
Henquesz <henquesvinicius@gmail.com>
2021-09-22 07:57:17 -0300

    [First Commit]

---
+++ CpuMonitor/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace CpuMonitor
+{
+    partial class form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pRAM = new System.Diagnostics.PerformanceCounter();
+            this.pCPU = new System.Diagnostics.PerformanceCounter();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarCPU = new MetroFramework.Controls.MetroProgressBar();
+            this.metroProgressBarRAM = new MetroFramework.Controls.MetroProgressBar();
+            this.lblCPU = new MetroFramework.Controls.MetroLabel();
+            this.lblRAM = new MetroFramework.Controls.MetroLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblNome = new MetroFramework.Controls.MetroLabel();
+            this.nome = new MetroFramework.Controls.MetroLabel();
+            this.btn_about = new MetroFramework.Controls.MetroButton();
+            this.btnLinkedin = new MetroFramework.Controls.MetroButton();
+            this.btnClear = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pRAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pRAM
+            // 
+            this.pRAM.CategoryName = "Memory";
+            this.pRAM.CounterName = "% Committed Bytes In Use";
+            // 
+            // pCPU
+            // 
+            this.pCPU.CategoryName = "Processor";
+            this.pCPU.CounterName = "% Processor Time";
+            this.pCPU.InstanceName = "_Total";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 800;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 89);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(71, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "CPU Level:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 139);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(96, 19);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "Memory Level:";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // metroProgressBarCPU
+            // 
+            this.metroProgressBarCPU.Location = new System.Drawing.Point(138, 89);
+            this.metroProgressBarCPU.Name = "metroProgressBarCPU";
+            this.metroProgressBarCPU.Size = new System.Drawing.Size(397, 23);
+            this.metroProgressBarCPU.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroProgressBarCPU.TabIndex = 2;
+            this.metroProgressBarCPU.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // metroProgressBarRAM
+            // 
+            this.metroProgressBarRAM.Cursor = System.Windows.Forms.Cursors.Default;
+            this.metroProgressBarRAM.Location = new System.Drawing.Point(138, 138);
+            this.metroProgressBarRAM.Name = "metroProgressBarRAM";
+            this.metroProgressBarRAM.Size = new System.Drawing.Size(397, 23);
+            this.metroProgressBarRAM.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroProgressBarRAM.TabIndex = 3;
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Location = new System.Drawing.Point(497, 67);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(27, 19);
+            this.lblCPU.TabIndex = 4;
+            this.lblCPU.Text = "0%";
+            this.lblCPU.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // lblRAM
+            // 
+            this.lblRAM.AutoSize = true;
+            this.lblRAM.Location = new System.Drawing.Point(497, 116);
+            this.lblRAM.Name = "lblRAM";
+            this.lblRAM.Size = new System.Drawing.Size(27, 19);
+            this.lblRAM.TabIndex = 5;
+            this.lblRAM.Text = "0%";
+            this.lblRAM.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Default;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(23, 191);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.LabelForeColor = System.Drawing.Color.White;
+            series7.Legend = "Legend1";
+            series7.Name = "CPU";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "RAM";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(408, 261);
+            this.chart1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.pictureBox1.Image = global::CpuMonitor.Properties.Resources.spaceman;
+            this.pictureBox1.Location = new System.Drawing.Point(352, 231);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 259);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.lblNome.Location = new System.Drawing.Point(154, 466);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(201, 15);
+            this.lblNome.TabIndex = 9;
+            this.lblNome.Text = "Computador/Usuario sendo analisado:";
+            // 
+            // nome
+            // 
+            this.nome.AutoSize = true;
+            this.nome.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.nome.ForeColor = System.Drawing.Color.Purple;
+            this.nome.Location = new System.Drawing.Point(358, 466);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(12, 15);
+            this.nome.TabIndex = 10;
+            this.nome.Text = "-";
+            // 
+            // btn_about
+            // 
+            this.btn_about.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_about.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            this.btn_about.Highlight = true;
+            this.btn_about.Location = new System.Drawing.Point(342, 268);
+            this.btn_about.Name = "btn_about";
+            this.btn_about.Size = new System.Drawing.Size(75, 23);
+            this.btn_about.Style = MetroFramework.MetroColorStyle.Purple;
+            this.btn_about.TabIndex = 11;
+            this.btn_about.Text = "About-me";
+            this.btn_about.UseSelectable = true;
+            this.btn_about.Click += new System.EventHandler(this.btn_about_Click);
+            // 
+            // btnLinkedin
+            // 
+            this.btnLinkedin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLinkedin.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            this.btnLinkedin.Highlight = true;
+            this.btnLinkedin.Location = new System.Drawing.Point(342, 297);
+            this.btnLinkedin.Name = "btnLinkedin";
+            this.btnLinkedin.Size = new System.Drawing.Size(75, 23);
+            this.btnLinkedin.Style = MetroFramework.MetroColorStyle.Purple;
+            this.btnLinkedin.TabIndex = 12;
+            this.btnLinkedin.Text = "My LinkedIn";
+            this.btnLinkedin.UseSelectable = true;
+            this.btnLinkedin.Click += new System.EventHandler(this.btnLinkedin_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            this.btnClear.Highlight = true;
+            this.btnClear.Location = new System.Drawing.Point(342, 239);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.Style = MetroFramework.MetroColorStyle.Purple;
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseSelectable = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(639, 485);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLinkedin);
+            this.Controls.Add(this.btn_about);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblRAM);
+            this.Controls.Add(this.lblCPU);
+            this.Controls.Add(this.metroProgressBarRAM);
+            this.Controls.Add(this.metroProgressBarCPU);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.MaximizeBox = false;
+            this.Name = "form";
+            this.Opacity = 0.9D;
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Purple;
+            this.Text = "CPU Monitoring";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pRAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter pRAM;
+        private System.Diagnostics.PerformanceCounter pCPU;
+        private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarCPU;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarRAM;
+        private MetroFramework.Controls.MetroLabel lblCPU;
+        private MetroFramework.Controls.MetroLabel lblRAM;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel lblNome;
+        private MetroFramework.Controls.MetroLabel nome;
+        private MetroFramework.Controls.MetroButton btn_about;
+        private MetroFramework.Controls.MetroButton btnLinkedin;
+        private MetroFramework.Controls.MetroButton btnClear;
+    }
+}
+
---
+++ CpuMonitor/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpuMonitor
+{
+    public partial class form : MetroFramework.Forms.MetroForm
+    {
+        public form()
+        {
+            // Vinicius Henques de Almeida - Aluno da Etec Horácio Augusto da Silveira e FullStack pela Fullture School
+            //Projeto dedicado ao enriquecimento do meu portfólio no linkedIn / GitHub.
+
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Variaveis de vaalor da cpu e memoria ram.
+            float fcpu = pCPU.NextValue();
+            float fram = pRAM.NextValue();
+
+            //Alteração da progress bar conforme os valores das variaveis.
+            metroProgressBarCPU.Value = (int)fcpu;
+            metroProgressBarRAM.Value = (int)fram;
+
+            //Alteração do texto de valor conforme os valores das variaveis.
+            lblCPU.Text = string.Format("{0:0.00}%", fcpu);
+            lblRAM.Text = string.Format("{0:0.00}%", fram);
+
+            //Aplicação dos valores no grafico chart atraveis de points.
+            chart1.Series["CPU"].Points.AddY(fcpu);
+            chart1.Series["RAM"].Points.AddY(fram);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Abertura do timer 
+            timer.Start();
+
+            // alteração da label variavel do nome, que armazena o nome do dispoitivo que esta sendo analisado.
+            nome.Text = (Environment.MachineName);
+        }
+        private void btn_about_Click(object sender, EventArgs e)
+        {
+            //MessageBox das minhas informações 
+            MessageBox.Show("Software desenvolvido pelo dev Vinicius Henques, Brasileiro, 17 anos e Fullstack Developer pela Etec Horácio & Fullture School \r\n \r\n Enjoy 😎 ");
+        }
+
+        private void btnLinkedin_Click(object sender, EventArgs e)
+        {
+            //Botão de redirecionamento pro meu LinkedIn
+            String myLinkedin = "https://www.linkedin.com/in/vinicius-henques-5a843a1b9/";
+            System.Diagnostics.Process.Start(myLinkedin);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Reinicialização / limpeza do grafico chart atraves de um foreach
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+    }
+}
